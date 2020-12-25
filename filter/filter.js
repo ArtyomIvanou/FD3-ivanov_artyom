f38d63860eb19d1a540bee95f152ecdaa4884927
ArtsiomIvanou <ArtyomIvanou@yandex.by>
2020-12-25 17:34:44 +0300

    4

--- filter/filter.js
+++ filter/filter.js
@@ -10,44 +10,37 @@ var Filter = React.createClass({
             textEmpty: [],
             checkedFilter: false,
             arr: this.props.catalogue,
-            arrayIsSorted: false,
+            arrayIsSorted: null,
             arrIsChanged: [],
             clearInput: '',
         };
     },
     setFilterAlphabet: function() {
-        /*if (this.state.arrayIsSorted) {
-            this.setState({ arrayIsSorted: false, })
-            return this.setState(this.state.arr)
-        }*/
         var newArr = []
-
-
         if (this.state.arrayIsSorted) {
             this.setState({ arrayIsSorted: false, })
-            this.setState(this.state.arr)
+            this.state.arr.forEach(function aaa(a) {
+                    newArr.push(a)
+                })
+                //alert(newArr)
+                //this.setState(this.state.arr)
+        } else {
+            this.setState({ arrayIsSorted: true, })
+            this.state.arr.forEach(function aaa(a) {
+                newArr.push(a)
+            })
+            newArr.sort(function(a, b) {
+                var nameA = a.toString(),
+                    nameB = b.toString()
+                if (nameA < nameB) //сортируем строки по возрастанию
+                    return -1
+                if (nameA > nameB)
+                    return 1
+                return 0 // Никакой сортировки
+            })
         }
-        this.state.arr.forEach(function aaa(a) {
-            newArr.push(a)
-        })
-        newArr.sort(function(a, b) {
-            var nameA = a.toString(),
-                nameB = b.toString()
-            if (nameA < nameB) //сортируем строки по возрастанию
-                return -1
-            if (nameA > nameB)
-                return 1
-            return 0 // Никакой сортировки
-        })
-        this.setState({ arrayIsSorted: true, })
-        return this.state.arr,
-            this.setState({ arr: newArr }),
-            alert(this.state.arr)
-
-        /*this.setState({ arrayIsSorted: true, })
-        this.setState({ arr: newArr })*/
+        this.setState({ arrIsChanged: newArr })
     },
-
     freeAnswerTextChanged: function(EO) {
 
         console.log(EO.target.value);
@@ -57,23 +50,35 @@ var Filter = React.createClass({
 
         })
         console.log(this.state.arr)
+        return this.state.arr
     },
     discard: function() {
         this.setState({
             arr: this.props.catalogue,
-            arrayIsSorted: false,
+            arrayIsSorted: null,
         })
+    },
+    resortSelect: function() {
+        alert('good')
     },
     render: function() {
         var productsCode = [];
+        if (this.state.arrayIsSorted == null) {
+            this.state.arr.forEach(function unitCreator(a) {
+                var unit = a;
+                var unitCode = React.DOM.option({ key: a, className: '' }, unit)
+                productsCode.push(unitCode);
+            });
+        } else {
+            this.state.arrIsChanged.forEach(function unitCreator(a) {
+                var unit = a;
+                var unitCode = React.DOM.option({ key: a, className: '' }, unit)
+                productsCode.push(unitCode);
+            });
+        }
 
-        this.state.arr.forEach(function unitCreator(a) {
-            var unit = a;
-            var unitCode = React.DOM.option({ key: a, className: '' }, unit)
-            productsCode.push(unitCode);
-        });
         return React.DOM.div({ className: 'Wrapper' }, React.DOM.div({ className: 'Filter' },
-            React.DOM.input({ type: 'checkbox', defaultChecked: this.state.arrayIsSorted, onChange: this.setFilterAlphabet }), React.DOM.input({ type: 'text', defaultValue: this.state.clearinput, onChange: this.freeAnswerTextChanged }, ), React.DOM.input({ type: 'button', value: 'Сброс', onClick: this.discard }),
-        ), React.DOM.select({ multiple: true, }, productsCode), );
+            React.DOM.input({ type: 'checkbox', defaultChecked: this.state.arrayIsSorted, onChange: this.setFilterAlphabet }), React.DOM.input({ type: 'text', /*defaultValue: this.state.clearinput,*/ onChange: this.freeAnswerTextChanged }, ), React.DOM.input({ type: 'button', value: 'Сброс', onClick: this.discard }),
+        ), React.DOM.select({ multiple: true, onChange: this.resortSelect, }, productsCode), );
     },
 });
