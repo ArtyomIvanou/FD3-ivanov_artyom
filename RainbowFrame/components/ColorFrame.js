36d6abf7a99d125c6b39d3f2e3a1e3457aebe5b8
ArtsiomIvanou <ArtyomIvanou@yandex.by>
2021-01-16 14:18:26 +0300

    16.01.21

---
+++ RainbowFrame/components/ColorFrame.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class ColorFrame extends React.Component {
+
+
+  render() {
+    var xxx = this.props.children
+    this.props.color.forEach(e => {
+      xxx = <div style={{ border: "solid 2px " + e, padding: "10px" }}>{xxx}</div>
+    });
+
+    return (xxx
+
+
+    );
+  }
+}
+
+export default ColorFrame;
---
+++ RainbowFrame/components/VotesBlock.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './VotesBlock.css';
+
+import VotesQuestion from './VotesQuestion';
+import VotesAnswer from './VotesAnswer';
+import ColorFrame from './ColorFrame';
+
+class VotesBlock extends React.Component {
+  render() {
+    let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+    return (
+        <ColorFrame color={colors}>
+         Hello
+        </ColorFrame>  
+    )
+    
+
+  }
+
+}
+
+export default VotesBlock;
