68033de87fe5db8bb8e9a6f9d91f2f29126d49c3
ArtsiomIvanou <ArtyomIvanou@yandex.by>
2021-01-17 16:18:31 +0300

    16,01,21

---
+++ br2jsx/components/ColorFrame.js
@@ -0,0 +1,30 @@
+﻿import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+class BR2JSX extends React.Component {
+
+
+  render() {
+    var reg=/<br [/]>|<br>|<br[/]>/
+    console.log(reg.test(this.props.text))
+    var res=[]
+    var arr=this.props.text.split(reg)
+    console.log(this.props.text.split(reg))
+   
+arr.map((e,i) => {
+  
+  res.push(e)
+  if (i!=arr.length-1) {
+    res.push(<br key={i}></br>)
+  }
+ 
+});
+   
+
+    return (
+      res
+    );
+  }
+}
+
+export default BR2JSX;
